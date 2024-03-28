4f71064c201dcd505804029df967340bc431ccd5
ChebbehAymen <devaymen15.5.3@gmail.com>
2024-03-28 14:18:15 +0000

    Added 404 error page

--- index.php
+++ index.php
@@ -6,4 +6,7 @@ $baseFolder = '/BP15/';
 switch (strtok($url, '?')){
     case $baseFolder:
         require $viewsFolder.'home.php';
+        break;
+    default:
+        require $viewsFolder.'404.php';
 }

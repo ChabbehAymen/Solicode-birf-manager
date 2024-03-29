df19d81a9f5685f704b4c71246ea0a2cebd190e0
TamraouiAbdelouahab <abdelwahabtamraoui@gmail.com>
2024-03-29 11:58:46 +0000

    added splashscreen

---
+++ views/splashscreen.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SplashScreen</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body style="background-color: rgb(227, 227, 227);overflow: hidden;">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-6">
+                <div>
+                    <img src="online-class-illustration-concept_52683-42416 2 (3).png" alt="ClassImage" class="w-100">
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="d-flex align-items-center justify-content-center" style="height: 100%;">
+                    <div>
+                        <strong>When You Love Your Class, Brifer Makes You Close To It.</strong>
+                        <p>Brifer is a Tool build to facilitate and boost student, former work.</p>
+                    </div>
+                </div>
+            </div>
+            <div class=" col-12">
+                <div class="d-flex flex-column  align-items-center justify-content-between" style="height: 100%;">
+                    <div class="p-3">
+                        <strong>“The whole purpose of education is to turn mirrors into windows.” —Sydney J. Harris</strong>
+                    </div>
+                    <div class="p-1">
+                        <button type="button" class="btn btn-primary">Start Brifing</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body> 
+</html>

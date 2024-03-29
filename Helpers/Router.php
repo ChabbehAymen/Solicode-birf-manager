7735b5bcee81ebdfd248fc0fb0db2c9b2a8a588e
ChebbehAymen <devaymen15.5.3@gmail.com>
2024-03-29 10:48:26 +0000

    Added login.php and configered it in the router and index.php

--- Helpers/Router.php
+++ Helpers/Router.php
@@ -2,7 +2,7 @@
 class Router
 {
     // this array hold every route page in the application
-    private static $routes = ['/'=>'home', 'error', '/BP15/404'];
+    private static $routes = ['/'=>'home', 'error'=>'/BP15/404','login'=>'/BP14/login' ];
 
     public static function route(string $destination, array $params = null ):void
     {

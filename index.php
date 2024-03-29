1cf36bbdc754ced3affaa73d8001bf18c65085ac
ChebbehAymen <devaymen15.5.3@gmail.com>
2024-03-29 12:09:59 +0000

    added imge and cofigured the index.php to require splashscreen.php

--- index.php
+++ index.php
@@ -4,6 +4,9 @@ $viewsFolder = './views/';
 $baseFolder = '/BP15/';
 
 switch (strtok($url, '?')){
+    case $baseFolder:
+        require $viewsFolder.'splashscreen.php';
+        break;
     case $baseFolder.'login':
         require $viewsFolder . 'login.php';
         break;
--- views/splashscreen.php
+++ views/splashscreen.php
@@ -6,7 +6,7 @@
     <title>SplashScreen</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body style="background-color: rgb(227, 227, 227);overflow: hidden;">
+<body style="overflow: hidden;">
     <div class="container-fluid">
         <div class="row">
             <div class="col-6">

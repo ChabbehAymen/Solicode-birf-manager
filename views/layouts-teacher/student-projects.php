ad26cf659cdcb4c12febf82d9dde3d24c26b81c2
aymen <devaymen15.5.3@gmail.com>
2024-04-07 00:13:47 +0000

    completed students projects page dsign

--- views/layouts-teacher/student-projects.php
+++ views/layouts-teacher/student-projects.php
@@ -1,1 +1,29 @@
-<?php var_dump($_GET['id']); ?>
+<div class="w-full mt-3 mb-11">
+    <div class="shadow-sm p-1 rounded-sm w-max ">
+        <i class="fa-solid fa-magnifying-glass" style="opacity:50%;"></i>
+        <input type="text" class="students-search-input" placeholder="Serach">
+    </div>
+</div>
+
+
+<!-- Project Card -->
+<div class="card rounded-lg shadow-sm relative " style="width: 20rem;">
+  <img src="https://placehold.co/600x400/png" class="card-img-top" >
+  <div class="absolute badge bg-blue-300 right-0 m-2 " >Doing</div>
+  <div class="card-body">
+    <h5 class="card-title mb-2 font-bold">Card title</h5>
+    <h1 class="card-subtitle mb-2" >8-10-2024</h1>
+    <h1 class="text-secondary" >Link: https://gitgub.com/user-git/user-repo</h1>
+    <hr class="mx-2 my-3" >
+    <h1 class="card-subtitle mb-2" >Competence</h1>
+    <p class="rounded-pill w-max border border-dark p-2">C1:maket</p>
+  </div>
+</div>
+
+<!-- <div class="rounded-lg w-max border shadow-sm card" style="width: 20rem;">
+    <img src="" class="card-img-top" >
+    <div class="card-body">
+        <h1 class="card-title">BP15</h1>
+        <h1 class="" >hoohlk</h1>
+    </div>
+</div> -->
